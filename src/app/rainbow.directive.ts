5ee871302769f37e10544b0a443b50ed8aa0baf3
David Allen <davetvmusic@hotmail.com>
2020-12-05 19:37:00 +1000

    add getter to HostBinding

--- src/app/rainbow.directive.ts
+++ src/app/rainbow.directive.ts
@@ -1,9 +1,12 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { style } from '@angular/animations';
+import { Directive, Host, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[rainbow]'
 })
 export class RainbowDirective {
+
+  @Input() bStyle: string;
 
   possibleColors = [
     'darksalmon', 'hotpink', 'lightskyblue', 'goldenrod', 'peachpuff',
@@ -13,6 +16,11 @@ export class RainbowDirective {
   @HostBinding('style.color') color: string;
   @HostBinding('style.borderColor') borderColor: string;
   @HostBinding('style.borderWidth') borderWidth = '5px';
+  @HostBinding('style.borderStyle') 
+  get style() {
+    return this.bStyle;
+  }
+
   @HostListener('keydown') newColor() {
     const colorPick = Math.floor(Math.random() * this.possibleColors.length);
     this.color = this.borderColor = this.possibleColors[colorPick];
